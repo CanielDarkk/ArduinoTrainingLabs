e42d3e35d671187fd9c950ad4b13efc949db57f8
duniel clunk <02daniel.clark@gulllakecs.org>
2019-12-04 14:18:50 -0500

    Potentiometer Working from 0 - 1023

--- Lab-1-Blink/Lab-1-Blink.ino
+++ Lab-1-Blink/Lab-1-Blink.ino
@@ -1,34 +1,25 @@
 /* Programmer: Daniel Clark
-    Date: 11.6.19
-    Program: digitalRead() & the Serial Port
+   Date: 12/3/9
+   Program: Potentiometer
 
-    Reads a digital input on pin2, prints the result to the serial monitor
+   AnalogReadSerial
+   Reads an analog input on pin A0, prints to the serial monitor
+   
 */
 
-//digital pin2 has a push button attached to it
-int pushButton = 2;
-
 void setup() {
-  // Using the Function pinMode to call the variable led and provide Output
-  pinMode(led, OUTPUT);
-
-  //initializes serial commmunication at 9600 bits per second
+  // initialize the serial communication at 9600 bits per second
   Serial.begin(9600);
-  //make the pushBotto pin an input
-  pinMode(pushButton, INPUT);
 }
 
 void loop() {
-  // read the input pin
-  int buttonState = digitalRead(pushButton);
-  // print out the state of the button
-  Serial.println(buttonState);
-  if (buttonState == HIGH) {
-    digitalWrite(led, HIGH);
-  }
-  else {
-    digitalWrite(led, LOW);
-  }
-  delay(1); // delay in between readings for stability
+  // read the input on analog pin 0
+  int potentiometerValue = analogRead(A0);
 
+  //  print the value of our potentiometer
+  Serial.println(potentiometerValue);
+
+  //add a delay for stability
+  delay(1);
+  
 }
