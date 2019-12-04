596ec7144d42039ebe00b825973a0d83b8e41389
duniel clunk <02daniel.clark@gulllakecs.org>
2019-12-04 13:48:30 -0500

    add If - Else for LED to go on and off based on buttonState

--- Lab-1-Blink/Lab-1-Blink.ino
+++ Lab-1-Blink/Lab-1-Blink.ino
@@ -1,20 +1,21 @@
 /* Programmer: Daniel Clark
- *  Date: 11.6.19
- *  Program: digitalRead() & the Serial Port
- *  
- *  Reads a digital input on pin2, prints the result to the serial monitor
- */
+    Date: 11.6.19
+    Program: digitalRead() & the Serial Port
+
+    Reads a digital input on pin2, prints the result to the serial monitor
+*/
 
 //digital pin2 has a push button attached to it
 int pushButton = 2;
 
 void setup() {
+  // Using the Function pinMode to call the variable led and provide Output
+  pinMode(led, OUTPUT);
+
   //initializes serial commmunication at 9600 bits per second
   Serial.begin(9600);
   //make the pushBotto pin an input
   pinMode(pushButton, INPUT);
-  
-  
 }
 
 void loop() {
@@ -22,6 +23,12 @@ void loop() {
   int buttonState = digitalRead(pushButton);
   // print out the state of the button
   Serial.println(buttonState);
-  delay(250); // delay in between readings for stability
+  if (buttonState == HIGH) {
+    digitalWrite(led, HIGH);
+  }
+  else {
+    digitalWrite(led, LOW);
+  }
+  delay(1); // delay in between readings for stability
 
 }
